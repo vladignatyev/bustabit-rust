5dea92b7f7ed9a15a6d0873a63e7df534373156c
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-01-26 13:18:05 +0400

    added doctests and meta for the cargo package

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! # Bustabit
+//!
+//! `bustabit` is a utility for verification of Bustabit games.
+//!
+//! Bustabit is a provably fair game of luck. “Provably fair” means that: 1) the outcome of every
+//! game round has not been changed after players placed a bet, and 2) this statement
+//! could be verified and proven by any third-party.
+//! Provably fair games rely on properties of cryptographical hash (one-way) functions.
+//! Bustabit’s Proof is proving additionally to independence of game outcome, the coefficient of outcome
+//! (called *a bust*) itself.
+//!
+//! You may use it if you want to get historical busts, or analyze games happened previously.
+//! Also, it may be useful while testing and debugging you autoplaying scripts.
+//!
+//! It is a pure Rust implementation of the 3rd party verification script: https://jsfiddle.net/Dexon95/2fmuxLza/embedded/result/
+
+
 #![allow(unused)]
 
 extern crate sha2;
@@ -13,14 +30,23 @@ use hmac::{Hmac, Mac};
 type HmacSha256 = Hmac<Sha256>;
 
 use std::i32;
+use std::fmt;
+
 
 #[derive(Debug)]
 pub struct Game {
-    pub hash: String,
-    _outcome: Option<f64>
+    pub hash: String
 }
 
 impl Game {
+    /// Creates new game from Game's hash
+    /// # Examples
+    ///
+    /// ```
+    /// let hash = String::from("5d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb2");
+    /// let game = bustabit::Game::new(&hash).unwrap();
+    /// println!("{}", game.outcome()) // will print 2055.9
+    /// ```
     pub fn new(s: &String) -> Option<Game> {
         if s.len() != 64 {
             return None
@@ -29,8 +55,7 @@ impl Game {
         match hex::decode(s) {
             Ok(val) => {
                 Some(Game {
-                    hash: s.to_lowercase(),
-                    _outcome: Option::None
+                    hash: s.to_lowercase()
                 })
             }
             Err(err) => {
@@ -39,12 +64,8 @@ impl Game {
         }
     }
 
-    pub fn outcome(&mut self) -> Option<f64> {
-        // Memoization
-        if self._outcome != Option::None {
-            return self._outcome;
-        }
-
+    /// Calculates and returns and outcome of the game
+    pub fn outcome(&self) -> f64 {
         // Bustabit seeding data
         const SEED:&'static [u8; 64] = b"0000000000000000004d6ec16dafe9d8370958664c1dc422f452892264c59526";
         const NUM_BITS:u32 = 52;
@@ -68,12 +89,8 @@ impl Game {
         let x = 99.0f64 / (1.0f64 - x1);
         let result:f64 = x.floor() * 0.01f64;
 
-        // truncate and store calculated value
-        let o = result.max(1.0f64);
-        self._outcome = Some(o);
-
-        // return value of outcome
-        self._outcome
+        // truncate and return calculated value
+        result.max(1.0f64)
     }
 }
 
@@ -83,6 +100,15 @@ impl PartialEq for Game {
     }
 }
 
+/// Iterator implementation that helps to navigate previous Bustabit games,
+/// so you can verify multiple games!
+/// # Examples
+/// ```
+/// # let hash = String::from("b2acd37fbdb5509926ab5d7329704c840f8467266c90019682f3b260a029bdba");
+/// let mut latest_game = bustabit::Game::new(&hash).unwrap();
+/// let mut previous_game = latest_game.next().unwrap();
+/// let one_before_previous = previous_game.next().unwrap();
+/// ```
 impl Iterator for Game {
     type Item = Game;
 
@@ -92,5 +118,11 @@ impl Iterator for Game {
         let result = hasher.result();
 
         Game::new(&format!("{:x}", result))
+    }
+}
+
+impl fmt::Display for Game {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Game’s hash: {} Bust: {}", self.hash, self.outcome())
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -51,15 +51,15 @@ fn should_return_correct_outcome() {
     let o = vec![1.26f64, 1.18f64, 1.26f64, 3.77f64];
 
     for i in 0..3 {
-        let mut h = Game::new(&String::from(s[i])).unwrap();
-        assert_eq!(h.outcome().unwrap(), o[i]);
+        let h = Game::new(&String::from(s[i])).unwrap();
+        assert!(h.outcome() - o[i] < std::f64::EPSILON);
     }
 }
 
 #[test]
 fn should_generate_correct_previous_games() {
     let s = "b2acd37fbdb5509926ab5d7329704c840f8467266c90019682f3b260a029bdba";
-    let expectedHashes = vec![
+    let expected_hashes = vec![
         "82886b71b3b26e4b162bbdf4e7024f50f6a7250c207fb9ce497ad56a3e7e700a",
         "cb7da3ec6cb68d4c0fccd9640641cabcac641f12ff97e7314ec3e074ac0981e0",
         "b459e199bac1342bf22d7aa6d19180aff35ab69f453d809949aab3e8d5e545aa"
@@ -69,9 +69,31 @@ fn should_generate_correct_previous_games() {
 
     let mut game1 = initial.next().unwrap();
     let mut game2 = game1.next().unwrap();
-    let mut game3 = game2.next().unwrap();
+    let game3 = game2.next().unwrap();
 
-    assert_eq!(game1.hash, expectedHashes[0]);
-    assert_eq!(game2.hash, expectedHashes[1]);
-    assert_eq!(game3.hash, expectedHashes[2]);
+    assert_eq!(game1.hash, expected_hashes[0]);
+    assert_eq!(game2.hash, expected_hashes[1]);
+    assert_eq!(game3.hash, expected_hashes[2]);
+}
+
+#[test]
+fn should_return_correct_outcome_for_nyans() {
+    let s = vec![
+        "5d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb2",
+        "7d58fa510e6ad19a5c2a08302906110702ad354c3d275592068bc3df9cd7fd1d",
+        "66508662791cbe526d740394a62713d3879f00cd11ee5ef10f7b592c676371f4"
+    ];
+
+    let o = vec![2055.9f64, 105710.05f64, 1456.85f64];
+
+    for i in 0..s.len() - 1 {
+        let h = Game::new(&String::from(s[i])).unwrap();
+        assert!(h.outcome() - o[i] < std::f64::EPSILON);
+    }
+}
+
+#[test]
+fn should_output_game_object() {
+    let game = Game::new(&String::from("5d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb2")).unwrap();
+    println!("{}", game);
 }
